badba77df62739fa2ebd1467a7a69dfffe710b03
mahedshah <mahedshah7@gmail.com>
2024-01-17 13:51:56 +0500

    commit

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,12 +1,23 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import axios from "axios";
 
 const MainComponent = () => {
-  const getAllNumbers = useCallback(() => {});
+  const [values, setvalues] = useState([]);
+  const getAllNumbers = useCallback(async () => {
+    const values = await axios.get("/api/values/all");
+    setvalues(values);
+  });
 
   return (
     <div>
       <button onClick={getAllNumbers}> Get all numbers</button>
+      <br />
+      <span>Values</span>
+      <div>
+        {values.map((value) => (
+          <div>{value}</div>
+        ))}
+      </div>
     </div>
   );
 };
